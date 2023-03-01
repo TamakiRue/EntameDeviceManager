eb7ed4f988d1a7df6db1058e74927933c5bdfd19
TamakiRue <tamaki.tatsuki@rue.co.jp>
2023-03-01 11:12:54 +0000

    Edit Home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,8 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                   You are logged in!
+                   Edit Test!!!
                 </div>
             </div>
         </div>
