28dcd93192fd4cb4916b6b3b23d8f07f60067983
tamaki.tatsuki@rue.co.jp <>
2023-03-20 20:21:28 +0900

    feat:Edit Entame_User Model

--- app/Entame_Users.php
+++ app/Entame_Users.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Entame_Users extends Model
 {
-    protected $fillable = ['name', 'email', 'address', 'tel']; 
+    protected $fillable = ['id', 'device_name', 'device_pattern', 'device_os','borrower','when_rented','created_at','updated_at']; 
 }
